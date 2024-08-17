a77ca09b9d7eaf447ef1c36b7d861500ca3900e4
Fran <franciscojaviermontielnoguera@gmail.com>
2024-08-17 18:53:53 +0200

    Algoritmos de ordenacion

---
+++ JavaScript - Algoritmos y estructuras de datos/14-Algoritmos_Clasificador_Numeros/script.js
@@ -0,0 +1,78 @@
+// DOM elements
+const sortButton = document.getElementById("sort");
+
+// Funciones
+const sortInputArray = (event) => {
+  event.preventDefault();
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown"),
+  ].map((dropdown) => Number(dropdown.value));
+
+  //const sortedValues = bubbleSort(inputValues);
+  //const sortedValues = selectionSort(inputValues);
+  //const sortedValues = insertionSort(inputValues);
+  const sortedValues = inputValues.sort((a, b) => a - b);
+  updateUI(sortedValues);
+};
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  });
+};
+
+/* 
+!  ALGORITMOS
+*/
+
+//bubble sort algorithm
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+};
+
+//selection sort algorithm
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+};
+
+//insertion sort algorithm
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+
+  return array;
+};
+
+sortButton.addEventListener("click", sortInputArray);
