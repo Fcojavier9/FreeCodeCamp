f990703b56eb3ce1bd03835fb85ec971da699f8d
Fran <franciscojaviermontielnoguera@gmail.com>
2024-09-04 12:21:38 +0200

    Logica programacional

---
+++ JavaScript - Algoritmos y estructuras de datos/20-Logica_Algoritmica_Juego_Dados/script.js
@@ -0,0 +1,48 @@
+// Elementos del DOM
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
+
+// Variables
+let diceValuesArr = [];
+let isModalShowing = false;
+let score = 0;
+let round = 1;
+let rolls = 0;
+
+// Eventos
+rulesBtn.addEventListener("click", () => {
+  isModalShowing = !isModalShowing;
+  rulesContainer.style.display = isModalShowing ? "block" : "none";
+  rulesBtn.innerText = isModalShowing ? "Hide rules" : "Show rules";
+});
+
+const rollDice = () => {
+  diceValuesArr = [];
+
+  for (let i = 0; i < 5; i++) {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    diceValuesArr.push(randomDice);
+  }
+
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
+};
+
+rollDiceBtn.addEventListener("click", () => {
+  if (rolls === 3) {
+    alert("You have made three rolls this round. Please select a score.");
+  } else {
+    rolls++;
+    rollDice();
+  }
+});
