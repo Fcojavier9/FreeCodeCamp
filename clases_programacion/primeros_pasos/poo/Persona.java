e8d93245eee11f581a51b589d01f0c0bf5651ed2
Fran <franciscojaviermontielnoguera@gmail.com>
2024-09-26 13:51:32 +0200

    Sexta clase

---
+++ clases_programacion/primeros_pasos/poo/Persona.java
@@ -0,0 +1,39 @@
+package primeros_pasos.poo;
+
+public class Persona {
+    // Propiedades / Atributos
+    public static final String PLANETA = "Tierra";
+    public String nombre;
+    public int edad;
+    public String dni;
+    public static String nacionalidad = "Española";
+
+    // constructor
+    public Persona(String name, int age, String dni) {
+        this.nombre = name;
+        this.edad = age;
+        this.dni = dni;
+    }
+
+    public Persona(String name, int age) {
+        this.nombre = name;
+        this.edad = age;
+        this.dni = "00000000A";
+    }
+
+    // metodos
+    public String toString(){
+        return "Nombre: " + this.nombre + "\nEdad: " + this.edad + "\nDNI: " + this.dni + "\nNacionalidad: " + nacionalidad;
+    }
+
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Edad: " + this.edad);
+        System.out.println("DNI: " + this.dni);
+        System.out.println("Nacionalidad: " + nacionalidad);
+    }
+
+    public static void holaMundo(){
+        System.out.println("Hola Mundo");
+    }
+}
